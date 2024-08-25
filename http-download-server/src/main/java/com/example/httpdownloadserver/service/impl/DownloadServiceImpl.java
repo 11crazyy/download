6a12ab54d112934d3edf2efcef467b4b3459907e
crazy2005 <2470345682@qq.com>
2024-08-25 23:15:47 +0800

    完善下载文件逻辑

--- http-download-server/src/main/java/com/example/httpdownloadserver/service/impl/DownloadServiceImpl.java
+++ http-download-server/src/main/java/com/example/httpdownloadserver/service/impl/DownloadServiceImpl.java
@@ -3,20 +3,17 @@ package com.example.httpdownloadserver.service.impl;
 import com.example.httpdownloadserver.dao.SettingsDAO;
 import com.example.httpdownloadserver.model.File;
 import com.example.httpdownloadserver.service.DownloadService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class DownloadServiceImpl implements DownloadService {
-    Logger logger = (Logger) LogManager.getLogger(DownloadServiceImpl.class);
 
     @Autowired
     private SettingsDAO settingsDAO;
@@ -36,25 +33,13 @@ public class DownloadServiceImpl implements DownloadService {
         int sliceSize = sliceSize(fileSize);
         //线程数
         int threadNum = Integer.parseInt(settingsDAO.selectByName("threadNum").getSettingValue());
-        //检查http状态
-        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-            //获取输入流
-            InputStream inputStream = connection.getInputStream();
-            //创建输出流
-            FileOutputStream outputStream = new FileOutputStream(settingsDAO.selectByName("downloadPath").getSettingValue());
-            //读取并写入数据
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            //关闭流
-            outputStream.close();
-            inputStream.close();
-            logger.info("fileDownloaded:" + file.getFileName());
-        } else {
-            logger.error("fileDownloadFailed:" + file.getFileName());
+        ExecutorService executor = Executors.newFixedThreadPool(threadNum);
+        for (int i = 0; i < threadNum; i++) {
+            long startIndex = (long) i * sliceSize;
+            long endIndex = (i == threadNum - 1) ? fileSize - 1 : startIndex + sliceSize - 1;
+            executor.execute(new DownloadTask(file.getUrl(), settingsDAO.selectByName("downloadPath").getSettingValue(), startIndex, endIndex));//线程逻辑：负责任务调度
         }
+        executor.shutdown();
         connection.disconnect();
     }
 
--- http-download-server/src/main/java/com/example/httpdownloadserver/service/impl/DownloadTask.java
+++ http-download-server/src/main/java/com/example/httpdownloadserver/service/impl/DownloadTask.java
@@ -1,21 +1,53 @@
 package com.example.httpdownloadserver.service.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class DownloadTask implements Runnable {
+    Logger logger = (Logger) LogManager.getLogger(DownloadTask.class);
+
     private String fileUrl;
     private String destination;
+    private Long startIndex;//开始下载位置
+    private Long endIndex;//结束下载位置
 
-    public DownloadTask(String fileUrl, String destination) {
+    public DownloadTask(String fileUrl, String destination, Long startIndex, Long endIndex) {
         this.fileUrl = fileUrl;
         this.destination = destination;
+        this.endIndex = endIndex;
+        this.startIndex = startIndex;
     }
 
+    //任务逻辑：下载任务
     @Override
     public void run() {
-        //任务逻辑：下载任务
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(fileUrl).openConnection();
+            connection.setRequestProperty("Range", "bytes=" + startIndex + "-" + endIndex);//设置下载范围
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();//从连接中读取下载的数据
+            RandomAccessFile raf = new RandomAccessFile(destination, "rw");//随机访问文件 将下载的数据写入到目标文件的特定位置
+            raf.seek(startIndex);//指定从哪个位置开始写入数据
+            //读取并写入数据
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                raf.write(buffer, 0, bytesRead);
+            }
+            //关闭流
+            raf.close();
+            inputStream.close();
+            logger.info("下载完成：" + startIndex + "-" + endIndex);
+            connection.disconnect();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
-//线程逻辑：在主线程或线程池中执行任务逻辑
-//    DownloadTask task = new DownloadTask("","");
-//    Thread thread = new Thread(task);
-//    thread.start();
-//}
+
